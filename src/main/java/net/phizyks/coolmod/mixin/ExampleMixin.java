188ede26c1527cb231a41aed3ecf32698811da27
phizyks <leopol192@yahoo.com>
2022-03-15 14:45:58 -0400

    added 3 "i"s

---
+++ src/main/java/net/phizyks/coolmod/mixin/ExampleMixin.java
@@ -0,0 +1,16 @@
+package net.phizyks.coolmod.mixin;
+///hiiii
+import net.minecraft.client.gui.screen.TitleScreen;
+import net.phizyks.coolmod.Cool_Mod;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TitleScreen.class)
+public class ExampleMixin {
+	@Inject(at = @At("HEAD"), method = "init()V")
+	private void init(CallbackInfo info) {
+		Cool_Mod.LOGGER.info("This line is printed by an example mod mixin!");
+	}
+}
