50fb5d8727d3e084a849bb0f0039949c05a13069
phizyks <leopol192@yahoo.com>
2022-03-15 16:28:38 -0400

    block

--- src/main/java/net/phizyks/coolmod/Cool_Mod.java
+++ src/main/java/net/phizyks/coolmod/Cool_Mod.java
@@ -1,6 +1,7 @@
 package net.phizyks.coolmod;
 
 import net.fabricmc.api.ModInitializer;
+import net.phizyks.coolmod.block.modblocks;
 import net.phizyks.coolmod.item.moditems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,6 +14,7 @@ public class Cool_Mod implements ModInitializer {
 	public void onInitialize() {
 
 		moditems.registermoditems();
+		modblocks.registerModBlocks();
 
 	}
 }
---
+++ src/main/java/net/phizyks/coolmod/block/modblocks.java
@@ -0,0 +1,36 @@
+package net.phizyks.coolmod.block;
+
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.Block;
+import net.minecraft.block.Material;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+import net.phizyks.coolmod.Cool_Mod;
+
+public class modblocks {
+
+    public static final Block MYTHRIL_BLOCK = registerBlock("mythril_block",
+            new Block(FabricBlockSettings.of(Material.METAL).strength(200f).requiresTool()), ItemGroup.MISC);
+
+
+
+
+
+    private static Block registerBlock(String name,Block  block, ItemGroup group) {
+        registerBlockItem(name block group);
+        return Registry.register(Registry.BLOCK, new Identifier(Cool_Mod.MOD_ID, name), block);
+    }
+
+    private static Item registerBlockItem(String name, Block block, ItemGroup group) {
+        return Registry.register(Registry.ITEM, new Identifier(Cool_Mod.MOD_ID, name),
+                new BlockItem(block, new FabricItemSettings().group(group)));
+    }
+
+    public static void registerModBlocks() {
+        Cool_Mod.LOGGER.info("Registering mod blocks for"+Cool_Mod.MOD_ID);
+    }
+}
